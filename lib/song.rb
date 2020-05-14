68cfcbb25d59cbafc4d1a044c0cb87a4467879c8
Christopher Lewis <cjoellewis@yahoo.com>
2020-05-14 01:11:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,5 +12,6 @@ class Song
   end
   
   def self.genre_count
-    @@genres.inject(Hash.new(0)) {|total, i| total[i] += 1 ;total
+    @@genres.inject(Hash.new(0)) {|total, i| total[i] += 1 ;total}
+  end
     
