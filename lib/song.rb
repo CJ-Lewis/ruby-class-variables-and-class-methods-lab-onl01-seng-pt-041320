0a573352d6c30e82573bb49fe2bece0e8e71f73a
Christopher Lewis <cjoellewis@yahoo.com>
2020-05-14 01:22:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,53 +1,41 @@
 class Song
-  attr_accessor :name, :artist, :genre
-  @@count = 0
+    attr_accessor :name, :artist, :genre
   
-  def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @genre = genre
-    @@genres << genre
-    @@count += 1
-  
-  def self.count
-    @@count = count
-  end
-  
-  @@genres = []
-  
-  def self.genres
-    @@genres.uniq
-  end
-  
-  def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre|
-      if genre_count[genre]
-        genre_count[genre] += 1
-      else
-        genre_count[genre] = 1
-      end
+    @@count = 0
+    @@artists = []
+    @@genres = []
+
+    def initialize(song_name,artist,genre)
+     @name = song_name
+     @artist = artist
+     @genre = genre
+     @@count += 1
+     @@artists << artist
+     @@genres << genre
     end
-    genre_count
-  end
-  
-  @@artists = []
-  
-  def self.artists
-    @@artists.uniq
-  end
-  
-  def self.artist_count
-    artist_count = {}
-    @@artists.each do |artist|
-      if artist_count[artist]
-        artist_count[artist] += 1
-      else
-        artist_count[artist] = 1
-      end
+
+    def self.count 
+     @@count
     end
-    artist_count
+
+    def self.artists
+     @@artists.uniq
+    end
+
+    def self.genres
+     @@genres.uniq
+    end
+
+    def self.genre_count
+     genre_count = Hash.new(0)
+     @@genres.each {|genre| genre_count[genre] += 1}
+     genre_count
+    end
+
+    def self.artist_count
+     artist_count = Hash.new(0)
+     @@artists.each {|artist| artist_count[artist] += 1}
+     artist_count
+    end
   end
-end
-end
     
